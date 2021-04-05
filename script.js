d887b12dec1eed02e8e95083c944a192656fd6a7
Jack Ritchie <jwf.ritchie@gmail.com>
2021-04-05 20:17:03 +1000

    final commit

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-var hourEL = moment().format("k");
-console.log(hourEL);
+$(document).ready(function() {
+
+$(".time").text(moment().format("dddd Do MMMM    - h[:]mma "))    
+var timeEl = moment().format("k");
+var rowTime = '';
+
+// console.log(timeEl); test time logg
+
+ $('.time-row-wrapper .row').each(function(){
+   var rowTime = $(this).attr('data-time');
+//    console.log(rowTime); test log
+   $('textarea' ,this).val(localStorage.getItem(rowTime));
+
+   if(timeEl > rowTime) {
+     $('textarea', this).addClass('past');  
+   }
+   else if (timeEl == rowTime) {
+    $('textarea', this).addClass('present');    
+   }
+   else {
+    $('textarea', this).addClass('future');      
+   }
+ });
+});
+
+$("button").on("click", function () {
+    var calendarEntry = $(this).parent('.row').attr('data-time');
+    console.log(calendarEntry);
+    var userInput = $(this).siblings('textarea').val();
+    console.log(userInput);
+    localStorage.setItem(calendarEntry, userInput);
+})
+
+
